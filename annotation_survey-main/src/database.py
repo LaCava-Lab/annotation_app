02d51ed84fb12e95e5847740d6f31916051c7473
ediefrederick <112941107+ediefrederick@users.noreply.github.com>
2024-12-13 13:04:50 -0500

    Add files via upload

---
+++ annotation_survey-main/src/database.py
@@ -0,0 +1,33 @@
+def connect():
+	'''
+	function to connect to S3 DB
+	WIP
+	'''
+	data_dir = "/Users/nikiathanasiadou/Documents/Projects/PaperAnnotation/python_code/streamlit_app/AWS_S3"
+	return data_dir
+
+def get_userDB():
+	'''
+	Load users table
+	''' 
+
+def get_papersDB():
+	'''
+	Load papers table
+	''' 
+
+def to_userDB():
+	'''
+	Update user's table 
+	''' 
+
+def to_papersDB():
+	'''
+	Update papers table
+	''' 
+
+def to_submit():
+	'''Submit the final, checked result'''
+
+def to_save():
+	''' Temporary save or update partial result'''
