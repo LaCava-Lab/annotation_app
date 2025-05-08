800dc90b7bdcd17f7baa28a83843826bc8bfba2b
Can <vlad.hazan.9.vh@gmail.com>
2025-05-09 00:01:49 +0200

    Bread Crumbs demo deactivated until pull request

--- pages/5_detail_picker.py
+++ pages/5_detail_picker.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from st_components.TableSelect import TableSelect
-from st_components.BreadCrumbs import BreadCrumbs
+# from st_components.BreadCrumbs import BreadCrumbs
 
 st.markdown(
     """
@@ -24,16 +24,16 @@ with st.sidebar:
     st.title("Paper Annotation")
     table = TableSelect()
 
-pageSelected = BreadCrumbs(links)
-
-if pageSelected == "Protocol Picker":
-    st.title("Protocol Picker")
-elif pageSelected == "Solution Picker":
-    st.title("Solution Picker")
-elif pageSelected == "Protocol Details":
-    st.title("Protocol Details")
-elif pageSelected == "Solution Details":
-    st.title("Solution Details")
-else:
-    st.title("Unknown Page")
+# pageSelected = BreadCrumbs(links)
+#
+# if pageSelected == "Protocol Picker":
+#     st.title("Protocol Picker")
+# elif pageSelected == "Solution Picker":
+#     st.title("Solution Picker")
+# elif pageSelected == "Protocol Details":
+#     st.title("Protocol Details")
+# elif pageSelected == "Solution Details":
+#     st.title("Solution Details")
+# else:
+#     st.title("Unknown Page")
 
