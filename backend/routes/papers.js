5082601002068f7823c7775e7cb520097c447000
IsmailMarghich <ismailcodes@gmail.com>
2025-07-17 13:18:03 +0200

    demo user will always get the same 5 papers

--- backend/routes/papers.js
+++ backend/routes/papers.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 const { Paper } = require('../models');
 
+const DEMO_EMAIL = 'demo@demo.com';
+const DEMO_PMIDS = ['37795087', '29309035', '22589133', '20021644', '20054825'];
+
 router.get('/', async (req, res) => {
-  const papers = await Paper.findAll();
-  res.json(papers);
+  try {
+    const isDemo = req.user?.UserEmail === DEMO_EMAIL;
+    console.log(req.user.UserEmail)
+    const papers = await Paper.findAll({
+      where: isDemo ? { PMID: DEMO_PMIDS } : {},
+    });
+
+    res.json(papers);
+  } catch (err) {
+    console.error('Error fetching papers:', err);
+    res.status(500).json({ error: 'Server error', details: err.message });
+  }
 });
 
 router.get('/:pmid', async (req, res) => {
